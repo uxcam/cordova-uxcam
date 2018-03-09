51baad1164b60c2454413469e7989e0af8cae580
Richard Groves <richard@noodlfroot.com>
2018-03-09 11:22:54 +0000

    Updating to iOS SDK 2.5.17

--- src/ios/UXCam.framework/CHANGELOG.md
+++ src/ios/UXCam.framework/CHANGELOG.md
@@ -1,53 +1,65 @@
-# UXCam iOS framwork
+# UXCam iOS framework
 
 ## Release Notes ##
 
-v2.5.8	- Trapping nil value for screen name that would cause a crash 
-		- Adding SetAutomaticScreenNameTagging: to disable automatic screen name capture
+Version | Changes
+------- | ----------
+2.5.17	| Fixing a problem with tag collection from multiple threads
+		| Fixing memory leaks when recording screens with video content
+2.5.16	| Fixing a 2.5.15 introduced bug with network reachability
+2.5.15	| Fixing some more iOS11 warnings about documentation in UXCam.h
+		| Problems when recording iPad apps in split mode have been fixed
+		| [BETA] `allowShortBreakForAnotherApp` method - will pause the recording while the user goes out to another.
+		| [BETA] `StopRecordingScrollingOnStutterOS` method to work around iOS11.2 problems
+2.5.14	| Fixing a warning in iOS11
+2.5.13	| Adjusting the occlusion of views during animations
+2.5.12	| Adjusting the occlusion of secure and/or UITextFields to be less sensitive to screen construction order
+2.5.11	| Removed the default setting of `tagUsersName` from UIDevice.currentDevice.name - there is no default now
+2.5.10	| Fixing a problem with screen names not being registered if no events occured on that screen
+2.5.9	| Adding `occludeAllTextFields` method
+		| Adding `SetMultiSessionRecord` and `GetMultiSessionRecord` methods
+		| Adding `PauseScreenRecording` and `ResumeScreenRecording` methods
+		| Adding `cancelCurrentSession` method
+		| Restoring the `DisableCrashHandling` method
+		| Adding method to access sessions awaiting upload. See `PendingUploads` for information
+		| Adding method to upload any pending sessions. See `UploadingPendingSessions` for details
+		| Improved occlusion of sensitive views in a moving scrollview
+2.5.8	| Trapping nil value for screen name that would cause a crash 
+		| Adding `SetAutomaticScreenNameTagging` to disable automatic screen name capture
+2.5.7	| Capturing app version as well as build number for dashboard
+2.5.6	| Fixing a problem with capturing Swift crashes
+		| Fixing a problem with `NSCameraUsageDescription` when submitting apps from XCode8
+		| Adding support for more quality settings
+		| Optimising some code paths
+2.5.5	| Fixing a bug that caused video problems on iOS8
+2.5.4	| Improvements in gesture timeline
+		| Improved capture of screen name at app start
+		| Better handling of sensitive views after app comes out of background
+		| Improved capture of crashed sessions
+2.5.3	| Don't occlude sensitive views that are hidden
+		| Fixing the `SessionURL` path
+2.5.2	| Fixing a 3G/wifi upload issue
+2.5.1	| Improving upload code
+2.5.0	| Refactoring of sending multiple sessions in one upload
+2.4.2	| Fixing user session URL
+2.4.1	| Change in how user country is captured
+2.4.0	| First version working as a Swift module
+		| Requires XCode7
+		| iOS7 is no longer tested against/supported, but should continue working
+2.3.4	| Changing timer resets
+2.3.3	| Adding verbose internal logging to SDK
+2.3.1	| Internal improvements
+2.3.0	| Ability to record just one session per run
+2.2.2	| Trapping some crash handler errors
+2.2.1	| Fixing log capture when more than one session in a run
+2.2.0	| Adding log output capture and upload
+2.1.1	| Fixing documenation links
+2.1.0	| Adding app variant support to `startUXCam` methods
+		| Exposing the occulde screen method to Cordova
+		| Fixing session and user URLs
+2.0.3	| Handle the case of an app having no icon file
+2.0.2	| Fixing some version number values in the uploaded data
+2.0.1	| Removed unused data fields from uploaded data
+2.0.0	| Major re-engineering of the SDK in terms of backend used
 
-v2.5.7	- Capturing app version as well as build number for dashboard
-v2.5.6	- Fixing a problem with capturing Swift crashes
-		- Fixing a problem with 'NSCameraUsageDescription' when submitting apps from XCode8
-		- Adding support for more quality settings
-		- Optimising some code paths
-
-v2.5.5	- Fixing a bug that caused video problems on iOS8
-
-v2.5.4  - Improvements in gesture timeline
-		- Improved capture of screen name at app start
-		- Better handling of sensitive views after app comes out of background
-		- Improved capture of crashed sessions
-
-v2.5.3	- Don't occlude sensitive views that are hidden
-		- Fixing the SessionURL path
-
-v2.5.2	- Fixing a 3G/wifi upload issue
-v2.5.1	- Improving upload code
-v2.5.0	- Refactoring of sending multiple sessions in one upload
-
-v2.4.2	- Fixing user session URL
-v2.4.1	- Change in how user country is captured
-v2.4.0	- First version working as a Swift module
-		- Requires XCode7
-		- iOS7 is no longer tested against/supported, but should continue working
-
-v2.3.4	- Changing timer resets
-v2.3.3	- Adding verbose internal logging to SDK
-v2.3.1	- Internal improvements
-v2.3.0	- Ability to record just one session per run
-
-
-v2.2.2	- Trapping some crash handler errors
-v2.2.1	- Fixing log capture when more than one session in a run
-v2.2.0	- Adding log output capture and upload
-
-v2.1.1	- Fixing documenation links
-v2.1.0	- Adding app variant support to startUXCam methods
-		- Exposing the occulde screen method to Cordova
-		- Fixing session and user URLs
-
-v2.0.3	- Handle the case of an app having no icon file
-v2.0.2	- Fixing some version number values in the uploaded data
-v2.0.1	- Removed unused data fields from uploaded data
-v2.0.0	- Major re-engineering of the SDK in terms of backend used
 
--- src/ios/UXCam.framework/Headers/UXCam.h
+++ src/ios/UXCam.framework/Headers/UXCam.h
@@ -1,9 +1,9 @@
 //
 //  UXCam.h
 //
-//  Copyright (c) 2013-2016 UXCam Ltd. All rights reserved.
+//  Copyright (c) 2013-2018 UXCam Ltd. All rights reserved.
 //
-//  UXCam SDK VERSION: 2.5.8
+//  UXCam SDK VERSION: 2.5.17
 //
 
 #import <Foundation/Foundation.h>
@@ -22,14 +22,14 @@ NS_ASSUME_NONNULL_BEGIN
  
 	@brief Start the UXCam session
 	@param userAPIKey			The key to identify your UXCam account - find it in the UXCam dashboard for your account at https://dashboard.uxcam.com/user/settings
-	@param appVariantIdentifier This string is added to the app bundle ID and name to differentiate builds of the same app on the UXCam dashboard - useful for seperating Debug and Release builds - pass nil for default values
-	@param multiSession			If YES (the default for other methods) then a new session is recorded each time the app comes to the foreground. IF NO then a single session is recorded, when stopped (either programmatically with @b stopApplicationAndUploadData or by the app going to the background) then no more sessions are recorded until @b startWithKey is called again)
-	@param completiongBlock		This block will be called once the app settings have been checked against the UXCam server - the parameter will be TRUE if recording has started
+	@param appVariant			This string is added to the app bundle ID and name to differentiate builds of the same app on the UXCam dashboard - useful for seperating Debug and Release builds - pass nil for default values
+	@param multiSession			If YES (the default for other methods) then a new session is recorded each time the app comes to the foreground. If NO then a single session is recorded, when stopped (either programmatically with @b stopApplicationAndUploadData or by the app going to the background) then no more sessions are recorded until @b startWithKey is called again)
+	@param sessionStartedBlock	This block will be called once the app settings have been checked against the UXCam server - the parameter will be TRUE if recording has started. @b NOTE @b: This block is captured and called each time a new session starts.
  */
 + (void) startWithKey:(NSString*)userAPIKey
  appVariantIdentifier:(nullable NSString*)appVariant
 	 multipleSessions:(BOOL)multiSession
-	  completionBlock:(nullable void (^)(BOOL started))block;
+	  completionBlock:(nullable void (^)(BOOL started))sessionStartedBlock;
 
 /**
 	Call this method from applicationDidFinishLaunching to start UXCam recording your application's session.
@@ -37,12 +37,12 @@ NS_ASSUME_NONNULL_BEGIN
  
 	@brief Start the UXCam session
 	@param userAPIKey			The key to identify your UXCam account - find it in the UXCam dashboard for your account at https://dashboard.uxcam.com/user/settings
-	@param appVariantIdentifier This string is added to the app bundle ID and name to differentiate builds of the same app on the UXCam dashboard - useful for seperating Debug and Release builds - pass nil for default values
-	@param completiongBlock		This block will be called once the app settings have been checked against the UXCam server - the parameter will be TRUE if recording has started
+	@param appVariant			This string is added to the app bundle ID and name to differentiate builds of the same app on the UXCam dashboard - useful for seperating Debug and Release builds - pass nil for default values
+	@param sessionStartedBlock	This block will be called once the app settings have been checked against the UXCam server - the parameter will be TRUE if recording has started.  @b NOTE @b: This block is captured and called each time a new session starts.
 */
 + (void) startWithKey:(NSString*)userAPIKey
  appVariantIdentifier:(NSString* _Nullable)appVariant
-	  completionBlock:(nullable void (^)(BOOL started))block;
+	  completionBlock:(nullable void (^)(BOOL started))sessionStartedBlock;
 
 /**
 	Call this method from applicationDidFinishLaunching to start UXCam recording your application's session.
@@ -50,7 +50,7 @@ NS_ASSUME_NONNULL_BEGIN
  
 	@brief Start the UXCam session
 	@param userAPIKey			The key to identify your UXCam account - find it in the UXCam dashboard for your account at https://dashboard.uxcam.com/user/settings
-	@param appVariantIdentifier This string is added to the app bundle ID and name to differentiate builds of the same app on the UXCam dashboard - useful for seperating Debug and Release builds - pass nil for default values
+	@param appVariant			This string is added to the app bundle ID and name to differentiate builds of the same app on the UXCam dashboard - useful for seperating Debug and Release builds - pass nil for default values
 */
 + (void) startWithKey:(NSString*)userAPIKey
  appVariantIdentifier:(nullable NSString*)appVariant;
@@ -74,16 +74,85 @@ NS_ASSUME_NONNULL_BEGIN
 + (void) stopApplicationAndUploadData;
 
 /**
+	Stops the UXCam application and sends captured data to the server.
+	Use this to start sending the data on UXCam server without the app going into the background.
+
+	@brief Stop the UXCam session and send data to the server
+
+	@param block Code block to call once upload process completes - will be run on the main thread. There might still be sessions waiting for upload if there was a problem with uploading any of them
+
+	@note This starts an asynchronous process and returns immediately.
+ */
++ (void) stopApplicationAndUploadData:(nullable void (^)(void))block;
+
+/**
+	By default UXCam will end a session immediately when your app goes into the background. But if you are switching over to another app for authorisation, or some other short action, and want the session to continue when the user comes back to your app then call this method with a value of TRUE before switching away to the other app.
+	UXCam will pause the current session as your app goes into the background and then continue the session when your app resumes. If your app doesn't resume within a couple of minutes the original session will be closed as normal and a new session will start when your app eventually is resumed.
+
+	@brief Prevent a short trip to another app causing a break in a session
+	@param continueSession Set to TRUE to continue the current session after a short trip out to another app. Default is FALSE - stop the session as soon as the app enters the background.
+	@note The flag is reset after a trip to another app - so the next time the user leaves your app the session will be closed unless you call this method again with TRUE
+ */
++ (void) allowShortBreakForAnotherApp:(BOOL)continueSession;
+
+/**
 	Starts a new session after the @link stopApplicationAndUploadData @/link method has been called.
 	This happens automatically when the app returns from background.
+ 
+	@note Any completion block registered during startWithKey: will be called as the session starts
 */
 + (void) restartSession;
+
+/**
+	Cancels the recording of the current session and discards the data
+ 
+	@note A new session will start as normal when the app nexts come out of the background (depending on the state of the MultiSessionRecord flag), or if you call @link restartSession @/link
+*/
++ (void) cancelCurrentSession;
+
+/**
+	Set whether to record multiple sessions or not
+ 
+	@param recordMultipleSessions YES to record a new session automatically when the device comes out of the background. If NO then a single session is recorded, when stopped (either programmatically with @b stopApplicationAndUploadData or by the app going to the background) then no more sessions are recorded until @b startWithKey is called again).
+	@note The default setting is to record a new session each time a device comes out of the background. This flag can be set to NO to stop that. You can also set this with the appropriate startWithKey: variant. (This will be reset each time startWithKey is called)
+*/
++ (void) SetMultiSessionRecord:(BOOL)recordMultipleSessions;
+
+/**
+	Get whether UXCam is set to automatically record a new session when the app resumes from the background
+*/
++ (BOOL) GetMultiSessionRecord;
+
 
 /**
 	When called the NSLog output (stderr) will be redirected to a file and uploaded with the session details
 	@note There will be no console output while debugging after calling this
 */
 + (void) CaptureLogOutput;
+
+/**
+ *  @brief Call this before calling startWithKey to disable UXCam from capturing sessions that crash
+ *
+ *  @param disable YES to disable crash capture
+ *  @note By default crashhandling is enabled
+ */
++ (void) DisableCrashHandling:(BOOL)disable;
+
+/**
+ * @brief Returns how many sessions are waiting to be uploaded
+ *
+ * Sessions can be in the Pending state if UXCam was unable to upload them at the end of the last session. Normally they will be sent at the end of the next session.
+ */
++ (NSUInteger) PendingUploads;
+
+/**
+ * @brief Begins upload of any pending sessions
+ *
+ * @param block Code block to call once upload process completes - will be run on the main thread. There might still be sessions waiting for upload if there was a problem with uploading any of them
+ *
+ * @note Advanced use only. This is not needed for most developers. This can't be called until UXCam startWithKey: has completed
+ */
++ (void) UploadingPendingSessions:(nullable void (^)(void))block;
 
 /**
 	Returns the current recording status
@@ -109,6 +178,14 @@ NS_ASSUME_NONNULL_BEGIN
 */
 + (void) occludeSensitiveScreen:(BOOL)hideScreen;
 
+/***
+	Hide / un-hide all UITextField views on the screen
+ 
+	@note Call this when you want to hide the contents of all UITextFields from the screen capture. Default is NO.
+ 
+	@param occludeAll Set YES to hide all UITextField views on the screen in the recording, NO to stop occluding them from the screen recording.
+ */
++ (void) occludeAllTextFields:(BOOL)occludeAll;
 
 /**
 	Enable / disable the automatic tagging of screen names
@@ -134,7 +211,8 @@ NS_ASSUME_NONNULL_BEGIN
 	UXCam uses a unique number to tag a device.
 	You can tag a device allowing you to search for it on the dashboard and review their sessions further.
 
-	@param username Name to apply to this user in this recording session
+	@param userName Name to apply to this user in this recording session
+	@note Starting with SDK v2.5.11 there is no default for this value - to have the previous behaviour call [UXCam tagUsersName:UIDevice.currentDevice.name];
 */
 +(void) tagUsersName:(NSString*)userName;
 
@@ -153,7 +231,7 @@ NS_ASSUME_NONNULL_BEGIN
  
 	@note Only NSNumber and NSString property types are supported to a maxiumum count of 100 and maximum size per entry of 1KiB
  */
-+ (void) addTag:(NSString*)tag withProperties:(nullable NSDictionary*)properties;
++ (void) addTag:(NSString*)tag withProperties:(nullable NSDictionary<NSString*, id>*)properties;
 
 /**
 	You can mark a session specifically if certain condition are met making them a good user for further testing.
@@ -181,12 +259,27 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (NSString*) urlForCurrentSession;
 
-#pragma mark - Deprecated methods - these will be removed on the next major version number update
+/**
+ *	Pauses the screen recording of the current session for the specified amount of time - specify 0 to pause indefinitely
+ *
+ *	@param pauseDuration Time (in seconds) to pause the screen, 0 for an indefinite pause
+ *	@note If the current session completes while paused the remaining pause time will be cancelled
+ */
++ (void) PauseScreenRecording:(NSTimeInterval)pauseDuration;
 
 /**
-	You can set the precise location of a user.
-*/
-+ (void) setPreciseLocationLatitude:(double)latitude longitude:(double)longitude __attribute__((deprecated("from SDK 1.0.9")));
+ *	Resumes a paused session - will cancel any remaining pause time and resume screen recording
+ */
++ (void) ResumeScreenRecording;
+
+
+/**
+ * [BETA] This is a workaround if you are having problems with stuttering scroll views in iOS11.2+
+ * Default is FALSE, but set TRUE to stop UXCam screen recording scrolling views
+ */
++ (void) StopRecordingScrollingOnStutterOS:(BOOL)stopScrollRecording;
+
+#pragma mark - Deprecated methods - these will be removed on the next major version number update
 
 /**
 	Call this method from applicationDidFinishLaunching to start UXCam recording your application's session.
