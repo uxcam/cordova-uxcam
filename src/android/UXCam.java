334b431eb09c5056adcbf4c2b9d7a9dc9e6f009c
samin20 <samin@uxcam.com>
2020-12-08 16:02:49 +0545

    add new APIs for android

--- src/android/UXCam.java
+++ src/android/UXCam.java
@@ -9,6 +9,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -117,6 +123,22 @@ public class UXCam extends CordovaPlugin {
             callbackContext.success(url);
         }  else if ("occludeRectsOnNextFrame".equals(action)) {
             com.uxcam.UXCam.occludeRectsOnNextFrame(new JSONArray(args.getString(0)));
+        } else if ("setPushNotificationToken".equals(action)) {
+            String token = args.getString(0);
+            com.uxcam.UXCam.setPushNotificationToken(token);
+        } else if ("reportBugEvent".equals(action)) {
+            String eventName = args.getString(0);
+            JSONObject params = args.getJSONObject(1);
+
+            if (eventName == null || eventName.length() == 0) {
+                throw new IllegalArgumentException("missing event Name");
+            }
+            if (params == null || params.length() == 0) {
+                com.uxcam.UXCam.reportBugEvent(eventName);
+            } else {
+                Map<String, Object> properties = toMap(params);
+                com.uxcam.UXCam.reportBugEvent(eventName, properties);
+            }
         } else {
             callbackContext.error("This API call is not supported by UXCam Android, API called: " + action);
             return false;
@@ -161,5 +183,33 @@ public class UXCam extends CordovaPlugin {
             }
         });
     }
-}
 
+    public static Map<String, Object> toMap(JSONObject jsonobj)  throws JSONException {
+        Map<String, Object> map = new HashMap<String, Object>();
+        Iterator<String> keys = jsonobj.keys();
+        while(keys.hasNext()) {
+            String key = keys.next();
+            Object value = jsonobj.get(key);
+            if (value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            } else if (value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            map.put(key, value);
+        }   return map;
+    }
+
+    public static List<Object> toList(JSONArray array) throws JSONException {
+        List<Object> list = new ArrayList<Object>();
+        for(int i = 0; i < array.length(); i++) {
+            Object value = array.get(i);
+            if (value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+            else if (value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            list.add(value);
+        }   return list;
+    }
+}
